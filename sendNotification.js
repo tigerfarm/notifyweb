ee1d3c8eae4ad4de57bd1981fd03faa57b0e81a7
Stacy David Thurston <tigerfarm@gmail.com>
2022-06-09 15:11:46 -0700

    update

--- sendNotification.js
+++ sendNotification.js
@@ -5,7 +5,7 @@ const notifyServiceSid = process.env.MASTER_NOTIFY_SID;
 console.log("+ Notify service SID: " + notifyServiceSid);
 client.notify.services(notifyServiceSid).notifications.create({
     // DeliveryCallbackUrl: 'https://example.com/notify',
-    identity: 'stacyw',      // davew stacyw 
-    body: 'Hello there 4'
+    identity: 'davec',      // davec davew stacyw 
+    body: 'Hello there 1c'
 }).then(notification => console.log("+ Sent: " + notification.sid))
         .catch(error => console.log(error));
